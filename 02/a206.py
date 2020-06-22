7e8f77b8b09fad8acea3238c4a1df2d9fda41f7e
gbanfi3 <gbanfi3@gmail.com>
2020-06-22 17:19:40 +0200

    boolean

--- 02/a206.py
+++ 02/a206.py
@@ -1,5 +1,11 @@
 friends = {'bob', 'rolf', 'anne'}
-abroad = {'bob', 'anne'}
+abroad = {'bob', 'anne', 'mary'}
 
 local_friends = friends.difference(abroad)
 print(local_friends)
+
+all_friends = friends.union(abroad)
+print(all_friends)
+
+intersection_friends = friends.intersection(abroad)
+print(intersection_friends)
