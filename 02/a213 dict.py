7f153da0df0474ebceeb39d0a149868a02d25ebb
gbanfi3 <gbanfi3@gmail.com>
2020-06-22 19:17:27 +0200

    dictionaries

--- 02/a213 dict.py
+++ 02/a213 dict.py
@@ -2,3 +2,6 @@ student_att = {"anna": 95,"bob":44, "adam":27}
 
 for i in student_att:
     print(f"neve: {i}, magassága: {student_att[i]}")
+
+for nev, magas in student_att.items():
+    print(f"{nev}: {magas}")
