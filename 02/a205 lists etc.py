b3e50f82dff778587cf86ab66032390112f1b06e
gbanfi3 <gbanfi3@gmail.com>
2020-06-22 17:09:50 +0200

    lists

---
+++ 02/a205 lists etc.py
@@ -0,0 +1,19 @@
+ll = ['bob', "rolf", "anne"]
+tt = ('bobb', 'rolff', 'annee')
+ss = {'bbob', 'rrolf', 'aanne', 'bbob'}
+print(ss)
+print(ll[1])
+
+print(ll)
+ll[1] = "aaaa"
+print(ll)
+#tt[1]=3
+
+ll.append('mégegy')
+print(ll)
+ll.remove("anne")
+print(ll)
+
+print(ss)
+ss.add("elefáni")
+print(ss)
