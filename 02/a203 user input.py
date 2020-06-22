6389674a0f541ca3ca8e8b5c2258d10466daeb37
gbanfi3 <gbanfi3@gmail.com>
2020-06-22 16:45:25 +0200

    first program

---
+++ 02/a203 user input.py
@@ -0,0 +1,10 @@
+# name = input("te ki vagy? ")
+# if name:
+#     print(f"én vagyok a {name} ember")
+#
+# he = int(name) / 10
+
+size_input = input("mekkora a házad? ")
+sq_feet = int(size_input)
+sq_meters = sq_feet / 10.8
+print(f"{sq_feet} feet2 az {sq_meters:.2f} m2")
