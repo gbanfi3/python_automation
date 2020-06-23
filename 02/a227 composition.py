b772f29dc0b07c87c7398f052004fb4e7ea7154c
gbanfi3 <gbanfi3@gmail.com>
2020-06-23 14:35:44 +0200

    nem teljesen az, mint a kurzusban, nálam list, ezért tudok hozzáadni új könyvet

--- 02/a227 composition.py
+++ 02/a227 composition.py
@@ -6,17 +6,21 @@ class Book:
         return f"könyv {self.name}"
 
 class Bookshelf:
-    def __init__(self, *books):
+    def __init__(self, *bookss):
         self.books = []
-        self.books.append(books)
+        for i in bookss:
+            self.books.append(i)
+        print(self)
 
     def __repr__(self):
-        return f"gyüjtemény, melyben {len(self.books)} van"
+        return f"gyüjtemény, melyben {len(self.books)} könyv van"
 
     def addbook(self, ujkonyv):
         self.books.append(ujkonyv)
 
 konyv1 = Book("harrypotter1")
 konyv2 = Book("harrypotter2")
+konyv3 = Book("harrypotter3")
 gyujtemeny = Bookshelf(konyv1, konyv2)
+gyujtemeny.addbook(konyv3)
 print(gyujtemeny)
