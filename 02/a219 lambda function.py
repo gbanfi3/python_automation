817a6ffe761115c4dae1c3d7617f2b6907e946ef
gbanfi3 <gbanfi3@gmail.com>
2020-06-23 08:52:29 +0200

    list comprehension

---
+++ 02/a219 lambda function.py
@@ -0,0 +1,14 @@
+szorzas = lambda a,b: a*b
+print(szorzas(2,3))
+
+aa = (lambda a,b: a*b)(3,4)
+print(aa)
+
+#list comprehension
+def doub(a):
+    return 2*a
+
+sequence = [1,2,3,4,5]
+doubled = [doub(x) for x in sequence]
+print(doubled)
+
