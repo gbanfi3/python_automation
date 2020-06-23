f10050c44369d1010697dedf1eb5cbfae6d55b81
gbanfi3 <gbanfi3@gmail.com>
2020-06-23 15:04:43 +0200

    másik

---
+++ 02/a228 type hinting.py
@@ -0,0 +1,6 @@
+from typing import List
+
+def listavg(sequence: List):
+    return sum(sequence) / len(sequence)
+
+listavg(123)
---
+++ 02/a229_2.py
@@ -0,0 +1,4 @@
+def divide(dividend, divisor):
+    return dividend/divisor
+
+print("a229_2.py: ", __name__)
