2f82cdd46f50e711934c5afcd1940c08e09c393a
gbanfi3 <gbanfi3@gmail.com>
2020-06-23 10:31:10 +0200

    return multiply(*para)
    kell a csillag!!!

--- 02/a221 arguments.py
+++ 02/a221 arguments.py
@@ -4,10 +4,10 @@ def multiply(*args):
         a *= i
     return a
 
-multiply(4,5,6)
-
-b = [1,2,3,4]
-multiply(b)
+# multiply(4,5,6)
+#
+# b = [1,2,3,4]
+# multiply(b)
 
 def add(*par):
     a = 0
@@ -15,13 +15,24 @@ def add(*par):
         a += i
     return a
 
-par = {'x':2, 'y':3}
-print(add(**par))
+print(add(5,6,7,8))
+aa = (5,6,7,8)
+print(add(*aa))
+
+# def add2(x,y):
+#     return x+y
+#
+# par = {'x':2, 'y':3}
+# print(add2(**par))
 
 def apply(*para, operator):
     if operator == '*':
-        return multiply(para)
+        return multiply(*para)
     elif operator == '+':
-        return add(para)
+        return add(*para)
     else:
-        print('nincs ilyen művelet')
+        return 'nincs ilyen művelet'
+
+print(apply(2,3,4,5,operator='*'))
+print(apply(2,3,4,5,operator='+'))
+print(apply(2,3,4,5,operator='>'))
