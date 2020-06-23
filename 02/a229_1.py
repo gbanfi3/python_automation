9b4542b6eb51d83b06f5d7689d18004c488c9558
gbanfi3 <gbanfi3@gmail.com>
2020-06-23 15:30:29 +0200

    mylib.py

--- 02/a229_1.py
+++ 02/a229_1.py
@@ -1,1 +1,4 @@
-from a229_2 import div
+from a229_2 import divide
+
+print(divide(3,5))
+print(f"a229_1.py: {__name__}")
--- 02/a229_2.py
+++ 02/a229_2.py
@@ -1,4 +1,6 @@
 def divide(dividend, divisor):
+    print("belső: ", __name__)
     return dividend/divisor
+
 
 print("a229_2.py: ", __name__)
