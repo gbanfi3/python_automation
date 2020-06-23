844db3af2c83316bb3e37237b7452e4dccb45def
gbanfi3 <gbanfi3@gmail.com>
2020-06-23 10:08:24 +0200

    **par, ha dictionary-ben vannak az argument értékek, pont olyan névvel, mint a fv. definícióban

--- 02/a221 arguments.py
+++ 02/a221 arguments.py
@@ -4,5 +4,14 @@ def multiply(*args):
         a *= i
     print(a)
 
+multiply(4,5,6)
+
 b = [1,2,3,4]
-multiply(*b)
+multiply(b)
+
+def add(x,y):
+    return x+y
+
+par = {'x':2, 'y':3}
+print(add(**par))
+
