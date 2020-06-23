58fe301995ac2778c1ca3e945ddeb436bc6a7082
gbanfi3 <gbanfi3@gmail.com>
2020-06-23 09:39:33 +0200

    sys.exit, ha nincs ilyen user

---
+++ 02/a220 dict comprehension.py
@@ -0,0 +1,21 @@
+import sys
+users = [(0,"aa", "11"),(1,"bb", "22"),(2,"cc", "33"),(3,"dd", "44")]
+
+for i in users:
+    print(i)
+
+user_mapping = { u[1]: u for u in users}
+print(user_mapping)
+
+input_username = input("mi a neved?: ")
+if not input_username in user_mapping.keys():
+    print("nincs ilyen user")
+    sys.exit(1)
+
+input_pw = input("password?: ")
+
+_, name, pw = user_mapping[input_username]
+if input_pw == pw:
+    print("ok")
+else:
+    print("nem OK")
