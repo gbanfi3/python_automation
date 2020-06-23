5a7382131b140bade44869dc062e5aac072df3cc
gbanfi3 <gbanfi3@gmail.com>
2020-06-23 17:45:25 +0200

    külső függvény lambda fv operator.itemgetter

--- 02/a233.py
+++ 02/a233.py
@@ -8,23 +8,34 @@
 #
 # c = calculate(2,0, operator=divide)
 # print(c)
+import typing
+from operator import itemgetter
+
+# from builtins import function
 
 friends = [
-    {'name': "aa bb", 'age':22}
-    {'name': "aa cc", 'age': 55}
-    {'name': "ff bb", 'age': 22}
-    {'name': "xx ee", 'age': 33}
+    {'name': "aa bb", 'age': 22},
+    {'name': "aa cc", 'age': 55},
+    {'name': "ff bb", 'age': 22},
+    {'name': "xx ee", 'age': 33},
     {'name': "ff mm", 'age': 22}
 ]
 
-def search(sequence, expected, finder):
+
+def search(sequence: typing.List, expected: str, finder):
     for i in sequence:
         if finder(i) == expected:
             return i
     raise RuntimeError(f"nem találtam meg őt: {expected}")
 
-def get_name(**a):
-    return a['name']
 
-a = search(friends, "ff bb",get_name)
+def get_name(a):
+    return a['name']
+a = search(friends, "ff bb", get_name)
+
+a = search(friends, "ff bb", itemgetter("name"))
+
+a = search(friends, "ff bb", lambda a: a["name"])
+
+
 print(a)
