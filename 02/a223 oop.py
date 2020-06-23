c92e0c49e06c5680699c601443f4814489819bab
gbanfi3 <gbanfi3@gmail.com>
2020-06-23 12:21:02 +0200

    methods

--- 02/a223 oop.py
+++ 02/a223 oop.py
@@ -1,6 +1,13 @@
 class Student:
     def __init__(self):
         self.name = "Rolf"
+        self.grades = (77,81,99,66,55)
+
+    def average(self):
+        return sum(self.grades) / len(self.grades)
 
 student = Student()
 print(student.name)
+print(student.grades)
+print(student.average())
+print(Student.average(student))
