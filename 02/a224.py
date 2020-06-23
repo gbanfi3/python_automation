f319a206f26c95d69a1adcb8ffa62fa02645b54f
gbanfi3 <gbanfi3@gmail.com>
2020-06-23 12:49:02 +0200

    classmethod, staticmethod

--- 02/a224.py
+++ 02/a224.py
@@ -7,7 +7,7 @@ class Person:
         return f"Hello, én vagyok {self.name} és {self.age} éves vagyok."
 
     def __repr__(self):
-        return f"<Person({self.name}, {self.age})>"
+        return f"<Person('{self.name}', {self.age})>"
 
 bob = Person("bob", 19)
 print(bob)
