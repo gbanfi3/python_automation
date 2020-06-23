4325d03b54345983e246d291696cec9de72b02f7
gbanfi3 <gbanfi3@gmail.com>
2020-06-23 10:11:33 +0200

    apply függvény

--- 02/a221 arguments.py
+++ 02/a221 arguments.py
@@ -2,16 +2,26 @@ def multiply(*args):
     a = 1
     for i in args:
         a *= i
-    print(a)
+    return a
 
 multiply(4,5,6)
 
 b = [1,2,3,4]
 multiply(b)
 
-def add(x,y):
-    return x+y
+def add(*par):
+    a = 0
+    for i in par:
+        a += i
+    return a
 
 par = {'x':2, 'y':3}
 print(add(**par))
 
+def apply(*para, operator):
+    if operator == '*':
+        return multiply(para)
+    elif operator == '+':
+        return add(para)
+    else:
+        print('nincs ilyen művelet')
