8492eb82cb56d1e8a611b31e2f5bb520fc7e841f
gbanfi3 <gbanfi3@gmail.com>
2020-06-23 13:46:14 +0200

    printer

--- 02/a226.py
+++ 02/a226.py
@@ -5,10 +5,22 @@ class Device:
         self.connected = True
 
     def __str__(self):
-        return f"Device {self.name}, {self.connected_by}"
+        return f"Device {self.name}, {self.connected_by}, {self.connected}"
 
     def disconnect(self):
         self.connected = False
+        print("disconnected")
+
+class Printer:
+    def __init__(self, name, connected_by, capacity):
+        super().__init__(name, connected_by)
+        self.capacity = capacity
+        remaining_pages = capacity
+
+    def __str__(self):
+        return f"printer vagyok, nevem {self.name}, conn {self.connected_by}, capacity {self.capacity}"
 
 printer = Device("laserprinter", "USB")
+print(printer)
+printer.disconnect()
 print(printer)
